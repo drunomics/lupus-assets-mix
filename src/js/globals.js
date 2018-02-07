a6e4d34b9f72edf882377a0427c5cb901ab409eb
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2018-02-07 19:57:15 +0100

    Add babel-polyfill.

--- src/js/globals.js
+++
@@ -1,6 +0,0 @@
-
-// Enable if jQuery is added.
-// window.$ = window.jQuery = require('jquery');
-
-import Vue from 'vue';
-window.Vue = Vue;
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import "babel-polyfill";
+import Vue from 'vue';
 
-require('./globals');
 
 // # Vue example usage:
 // Vue.component('example', require('./Example.vue'));
